96f4fb0016eeba795c2e6bdac89c927d0386511a
Miauri <mauriciotorrez00@gmail.com>
2025-09-03 21:09:07 -0400

    Conexion bluetooth por joystick (brazos)

--- src/infonor.py
+++ src/infonor.py
@@ -8,6 +8,49 @@ import atexit
 from IK import Pierna
 
 pg.init()
+# This dict can be left as-is, since pygame will generate a
+# pygame.JOYDEVICEADDED event for every joystick connected
+# at the start of the program.
+joysticks = {}
+pause = False
+
+
+def pygane_event_handle():
+    global pause, start_time
+    # Event processing step.
+    # Possible joystick events: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
+    # JOYBUTTONUP, JOYHATMOTION, JOYDEVICEADDED, JOYDEVICEREMOVED
+    for event in pg.event.get():
+        if event.type == pg.JOYBUTTONDOWN:
+            print("Joystick button pressed.")
+            pause = not pause
+            if pause == False:
+                pause = True
+            else:
+                start_time = time.time()
+                pause = False
+            if event.button == 0:
+                joystick = joysticks[event.instance_id]
+                if joystick.rumble(0, 0.7, 500):
+                    print(
+                        f"Rumble effect played on joystick {event.instance_id}")
+
+        if event.type == pg.JOYBUTTONUP:
+            print("Joystick button released.")
+
+        # Handle hotplugging
+        if event.type == pg.JOYDEVICEADDED:
+            # This event will be generated when the program starts for every
+            # joystick, filling up the list without needing to create them manually.
+            joy = pg.joystick.Joystick(event.device_index)
+            joysticks[joy.get_instance_id()] = joy
+            print(f"Joystick {joy.get_instance_id()} connencted")
+
+        if event.type == pg.JOYDEVICEREMOVED:
+            del joysticks[event.instance_id]
+            print(f"Joystick {event.instance_id} disconnected")
+
+
 clock = pg.time.Clock()
 
 
@@ -24,6 +67,7 @@ def chau(kit: ServoKit):
     print("APAGANDO SERVOS...")
     kit.servo[0].angle = 90
     kit.servo[4].angle = 90
+    pg.quit()
     # for i in range(16):
     #     kit.servo[i].angle = 90
     # time.sleep(0.5)
@@ -54,49 +98,64 @@ while True:
     # kit.servo[14].angle = 90  # hombro der
     # kit.servo[11].angle = 180-40  # brazo der
 
-    servos = [
-        kit.servo[3],  # cadera izq  (0)
-        kit.servo[2],  # rodilla izq (1)
-        kit.servo[1],  # tobillo izq (2)
-        kit.servo[0],  # pie izq     (3)
+    # servos = [
+    #     kit.servo[3],  # cadera izq  (0)
+    #     kit.servo[2],  # rodilla izq (1)
+    #     kit.servo[1],  # tobillo izq (2)
+    #     kit.servo[0],  # pie izq     (3)
 
-        kit.servo[7],  # cadera der  (4)
-        kit.servo[6],  # rodilla der (5)
-        kit.servo[5],  # tobillo der (6)
-        kit.servo[4],  # pie der     (7)
+    #     kit.servo[7],  # cadera der  (4)
+    #     kit.servo[6],  # rodilla der (5)
+    #     kit.servo[5],  # tobillo der (6)
+    #     kit.servo[4],  # pie der     (7)
 
-        kit.servo[13],  # hombro izq (8)
-        kit.servo[12],  # brazo izq  (9)
+    #     kit.servo[13],  # hombro izq (8)
+    #     kit.servo[12],  # brazo izq  (9)
 
-        kit.servo[14],  # hombro der (10)
-        kit.servo[11],  # brazo der  (11)
-    ]
+    #     kit.servo[14],  # hombro der (10)
+    #     kit.servo[11],  # brazo der  (11)
+    # ]
 
-    Y = np.zeros(shape=len(servos))
-    start = time.time()
+    # Y = np.zeros(shape=len(servos))
+    start_time = time.time()
     t = 0
-    freq = 0.2  # 1.2
+    freq = 0.3  # 1.2
     BALANCEO = 17  # 0.2  # 0.105
-    ZANCADA = 2  # 0.2  # 0.09
-    SENTADILLA = 0.4
-    RESORTE = 0.1
+    ZANCADA = 4  # 0.2  # 0.09
+    # SENTADILLA = 0.4
+    # RESORTE = 0.1
 
-    INCLINACION = 0.1
+    # INCLINACION = 0.1
     dt = 0
-    while True:
-        t = time.time() - start
-        fase = t*freq*2*np.pi
-        print(f"{t = }")
 
-        pierna_izq.angulos(ZANCADA * np.cos(fase),
-                           13 + np.sin(fase),
-                           a=7,
-                           p=(BALANCEO*1.3)*np.sin(fase) - 35*np.sin(fase/2)**10)
-        pierna_der.angulos(-ZANCADA * np.cos(fase),
-                           13 - np.sin(fase),
-                           a=7,
-                           p=-BALANCEO*np.sin(fase) - 35*np.cos(fase/2)**10)
-        dt = clock.tick(100) / 1000
+    while True:
+        if not pause:
+            t = time.time() - start_time
+        fase = t*freq*2*np.pi
+        # print(f"{t = }")
+
+        # pierna_izq.angulos(ZANCADA * np.cos(fase),
+        #                    12 + np.sin(fase),
+        #                    a=8,
+        #                    p=(BALANCEO*1.3)*np.sin(fase) - 30*np.sin(fase/2)**10)
+        # pierna_der.angulos(-ZANCADA * np.cos(fase),
+        #                    12 - np.sin(fase),
+        #                    a=8,
+        #                    p=-BALANCEO*np.sin(fase) - 30*np.cos(fase/2)**10)
+
+        pygane_event_handle()
+        for joystick in joysticks.values():
+            # Eje 0:1 joystick izquierdo
+            # Eje 2:3 joystick derecho
+            kit.servo[8].angle = 90 + joystick.get_axis(1) * 80
+            kit.servo[9].angle = 90 + joystick.get_axis(0) * 80
+            kit.servo[10].angle = 90 - joystick.get_axis(4) * 80
+            kit.servo[11].angle = 90 + joystick.get_axis(3) * 80
+            for i in range(joystick.get_numaxes()):
+                axis = joystick.get_axis(i)
+                print(f"Eje {i}: {axis:.2f}", end="\t")
+            print()
+        dt = clock.tick(60) / 1000
         # char = readchar.readkey()
         # t += (1/40) * (char == readchar.key.RIGHT)
         # t -= (1/40) * (char == readchar.key.LEFT)
